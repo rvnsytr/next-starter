66b0fcc29fbc207994b98c31efc5e276ae389db6
RvnSytR <ravenskytd17@gmail.com>
2025-12-08 16:34:12 +0700

    fix: api config

--- core/constants/app.ts
+++ core/constants/app.ts
@@ -3,15 +3,10 @@ export const appMeta = {
   description: "App description...",
   keywords: ["next", "next starter"],
 
+  lang: "id",
+
   // logo: {
   //   default: "/logo.png",
   //   withText: "/logo-text.png",
   // },
-
-  lang: "id",
-
-  apiHost:
-    process.env.NODE_ENV === "production"
-      ? "https://apidomain.example/api/v1"
-      : "http://localhost:8000/api/v1",
 };
