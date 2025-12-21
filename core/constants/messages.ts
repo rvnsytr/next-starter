8ef730e3ef0d2ce90ff6d18b7e025baefe687cb5
RvnSytR <ravenskytd17@gmail.com>
2025-12-21 19:07:57 +0700

    fix: remove ban action message

--- core/constants/messages.ts
+++ core/constants/messages.ts
@@ -10,7 +10,6 @@ export const messages = {
     action: "Tindakan",
     add: "Tambah",
     back: "Kembali",
-    ban: "Blokir",
     cancel: "Batal",
     clear: "Bersihkan",
     confirm: "Konfirmasi",
