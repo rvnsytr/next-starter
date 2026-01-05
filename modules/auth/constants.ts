3dd7ef9076d57fc0f494d763c62207d7ce5a779b
RvnSytR <ravenskytd17@gmail.com>
2026-01-05 11:41:47 +0700

    fix: misc

--- modules/auth/constants.ts
+++ modules/auth/constants.ts
@@ -9,8 +9,8 @@ import {
 } from "lucide-react";
 
 export type AuthSession = typeof auth.$Infer.Session;
-
 export type Role = keyof typeof roles;
+
 export const allRoles = Object.keys(roles) as Role[];
 export const defaultRole: Role = "user";
 
