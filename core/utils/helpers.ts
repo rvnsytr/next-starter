229720fca18fb129b42affd6f1e6df24ee1aeef6
RvnSytR <ravenskytd17@gmail.com>
2026-01-28 10:46:18 +0700

    feat: import dialog

--- core/utils/helpers.ts
+++ core/utils/helpers.ts
@@ -9,6 +9,7 @@ import {
   RouteRole,
   routesMeta,
 } from "../constants";
+import { sharedSchemas } from "../schema.zod";
 
 export function authorizedRoute(route: Route | null, role?: Role) {
   if (!route || !role) return false;
@@ -59,6 +60,72 @@ export function getRandomColor(withHash?: boolean) {
   let color = withHash ? "#" : "";
   for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
   return color;
+}
+
+export function getExcelColumnKey(columnNumber: number): string {
+  if (columnNumber < 0 || !Number.isInteger(columnNumber)) return "-";
+
+  let result = "";
+  let n = columnNumber;
+
+  while (n > 0) {
+    n--;
+    result = String.fromCharCode((n % 26) + 65) + result;
+    n = Math.floor(n / 26);
+  }
+
+  return !!result ? result : "-";
+}
+
+export type ParseCsvRangeOptions = {
+  sort?: "asc" | "desc";
+  distinct?: boolean;
+  exclude?: number[];
+};
+
+const parseCsvRangesSchema = sharedSchemas.number("Baris").int().positive();
+
+export function parseCsvRanges(
+  input: string,
+  options: ParseCsvRangeOptions = {},
+): number[] {
+  const { sort, distinct = false, exclude = [] } = options;
+
+  const excludeSet = new Set(exclude);
+
+  const result: number[] = [];
+  const tokens = input.split(",");
+
+  for (const token of tokens) {
+    const trimmed = token.trim();
+
+    if (trimmed.includes("-")) {
+      const [startStr, endStr] = trimmed.split("-");
+
+      const start = Number(startStr);
+      const end = Number(endStr);
+
+      if (
+        parseCsvRangesSchema.safeParse(start).success &&
+        parseCsvRangesSchema.safeParse(end).success &&
+        start <= end
+      ) {
+        for (let i = start; i <= end; i++)
+          if (!excludeSet.has(i)) result.push(i);
+      }
+
+      continue;
+    }
+
+    const value = Number(trimmed);
+    if (parseCsvRangesSchema.safeParse(value).success && !excludeSet.has(value))
+      result.push(value);
+  }
+
+  const finalResult = distinct ? Array.from(new Set(result)) : result;
+  if (sort) finalResult.sort((a, b) => (sort === "asc" ? a - b : b - a));
+
+  return finalResult;
 }
 
 export function getMenuByRole(
