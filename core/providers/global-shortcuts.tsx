fd7f75bbbcf1193a2da2949e9470c9c8e20e7114
RvnSytR <ravenskytd17@gmail.com>
2026-04-20 20:17:09 +0700

    feat: use view transitions

--- core/providers/global-shortcuts.tsx
+++ core/providers/global-shortcuts.tsx
@@ -3,19 +3,21 @@
 import { useHotkeys } from "@tanstack/react-hotkeys";
 import { useTheme } from "next-themes";
 import { nextTheme, THEME_TOGGLE_HOTKEY } from "../components/theme";
+import { useViewTransition } from "../hooks/use-view-transition";
 
 export function GlobalShortcuts() {
   const { setTheme } = useTheme();
+  const { isTransitioning, startTransition } = useViewTransition();
 
-  useHotkeys([
-    {
-      hotkey: THEME_TOGGLE_HOTKEY,
-      callback: () => {
-        if (!document.startViewTransition) return setTheme(nextTheme);
-        document.startViewTransition(() => setTheme(nextTheme));
+  useHotkeys(
+    [
+      {
+        hotkey: THEME_TOGGLE_HOTKEY,
+        callback: () => startTransition(() => setTheme(nextTheme)),
       },
-    },
-  ]);
+    ],
+    { enabled: !isTransitioning },
+  );
 
   return null;
 }
