87f02b10eb45b214809af09294daba0cbd181b00
RvnSytR <ravenskytd17@gmail.com>
2026-05-01 09:31:51 +0700

    chore: to drizzle rc 1

--- shared/db/schema.ts
+++ shared/db/schema.ts
@@ -3,30 +3,30 @@ import {
   bigint,
   boolean,
   index,
-  pgTable,
+  snakeCase,
   text,
   timestamp,
   uuid,
 } from "drizzle-orm/pg-core";
 import { allRoles } from "../permission";
 
-export const user = pgTable(
+export const user = snakeCase.table(
   "user",
   {
-    id: text("id").primaryKey(),
-    name: text("name").notNull(),
-    email: text("email").notNull().unique(),
-    emailVerified: boolean("email_verified").default(false).notNull(),
-    image: text("image"),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at")
+    id: text().primaryKey(),
+    name: text().notNull(),
+    email: text().notNull().unique(),
+    emailVerified: boolean().default(false).notNull(),
+    image: text(),
+    createdAt: timestamp().defaultNow().notNull(),
+    updatedAt: timestamp()
       .defaultNow()
       .$onUpdate(() => /* @__PURE__ */ new Date())
       .notNull(),
-    role: text("role", { enum: allRoles }).default("user").notNull(),
-    banned: boolean("banned").default(false),
-    banReason: text("ban_reason"),
-    banExpires: timestamp("ban_expires"),
+    role: text({ enum: allRoles }).default("user").notNull(),
+    banned: boolean().default(false),
+    banReason: text(),
+    banExpires: timestamp(),
   },
   (table) => [
     index("user_role_idx").on(table.role),
@@ -34,59 +34,59 @@ export const user = pgTable(
   ],
 );
 
-export const session = pgTable(
+export const session = snakeCase.table(
   "session",
   {
-    id: text("id").primaryKey(),
-    expiresAt: timestamp("expires_at").notNull(),
-    token: text("token").notNull().unique(),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at")
+    id: text().primaryKey(),
+    expiresAt: timestamp().notNull(),
+    token: text().notNull().unique(),
+    createdAt: timestamp().defaultNow().notNull(),
+    updatedAt: timestamp()
       .$onUpdate(() => /* @__PURE__ */ new Date())
       .notNull(),
-    ipAddress: text("ip_address"),
-    userAgent: text("user_agent"),
-    userId: text("user_id")
+    ipAddress: text(),
+    userAgent: text(),
+    userId: text()
       .notNull()
       .references(() => user.id, { onDelete: "cascade" }),
-    impersonatedBy: text("impersonated_by"),
+    impersonatedBy: text(),
   },
   (table) => [index("session_user_id_idx").on(table.userId)],
 );
 
-export const account = pgTable(
+export const account = snakeCase.table(
   "account",
   {
-    id: text("id").primaryKey(),
-    accountId: text("account_id").notNull(),
-    providerId: text("provider_id").notNull(),
-    userId: text("user_id")
+    id: text().primaryKey(),
+    accountId: text().notNull(),
+    providerId: text().notNull(),
+    userId: text()
       .notNull()
       .references(() => user.id, { onDelete: "cascade" }),
-    accessToken: text("access_token"),
-    refreshToken: text("refresh_token"),
-    idToken: text("id_token"),
-    accessTokenExpiresAt: timestamp("access_token_expires_at"),
-    refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
-    scope: text("scope"),
-    password: text("password"),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at")
+    accessToken: text(),
+    refreshToken: text(),
+    idToken: text(),
+    accessTokenExpiresAt: timestamp(),
+    refreshTokenExpiresAt: timestamp(),
+    scope: text(),
+    password: text(),
+    createdAt: timestamp().defaultNow().notNull(),
+    updatedAt: timestamp()
       .$onUpdate(() => /* @__PURE__ */ new Date())
       .notNull(),
   },
   (table) => [index("account_user_id_idx").on(table.userId)],
 );
 
-export const verification = pgTable(
+export const verification = snakeCase.table(
   "verification",
   {
-    id: text("id").primaryKey(),
-    identifier: text("identifier").notNull(),
-    value: text("value").notNull(),
-    expiresAt: timestamp("expires_at").notNull(),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at")
+    id: text().primaryKey(),
+    identifier: text().notNull(),
+    value: text().notNull(),
+    expiresAt: timestamp().notNull(),
+    createdAt: timestamp().defaultNow().notNull(),
+    updatedAt: timestamp()
       .defaultNow()
       .$onUpdate(() => /* @__PURE__ */ new Date())
       .notNull(),
@@ -94,40 +94,40 @@ export const verification = pgTable(
   (table) => [index("verification_identifier_idx").on(table.identifier)],
 );
 
-export const file = pgTable(
+export const file = snakeCase.table(
   "file",
   {
-    id: uuid("id").primaryKey().defaultRandom(),
+    id: uuid().primaryKey().defaultRandom(),
 
-    filePath: text("file_path").notNull(),
-    fileName: text("file_name").notNull(),
-    mimeType: text("mime_type").notNull(),
-    fileSize: bigint("file_size", { mode: "number" }).notNull(),
+    filePath: text().notNull(),
+    fileName: text().notNull(),
+    mimeType: text().notNull(),
+    fileSize: bigint({ mode: "number" }).notNull(),
 
-    updatedAt: timestamp("updated_at")
+    updatedAt: timestamp()
       .defaultNow()
       .$onUpdate(() => /* @__PURE__ */ new Date())
       .notNull(),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
+    createdAt: timestamp().defaultNow().notNull(),
   },
   (table) => [index("file_file_path_idx").on(table.filePath)],
 );
 
 export type Activity = typeof activity.$inferSelect;
 export type ActivityWithEntity = Activity & { entity?: string };
-export const activity = pgTable(
+export const activity = snakeCase.table(
   "activity",
   {
-    id: uuid("id").primaryKey().defaultRandom(),
-    userId: text("user_id")
+    id: uuid().primaryKey().defaultRandom(),
+    userId: text()
       .notNull()
       .references(() => user.id, { onDelete: "cascade" }),
 
-    type: text("type", { enum: allActivityType }).notNull(),
-    entityId: text("entityId"),
-    data: text("data"),
+    type: text({ enum: allActivityType }).notNull(),
+    entityId: text(),
+    data: text(),
 
-    createdAt: timestamp("created_at").defaultNow().notNull(),
+    createdAt: timestamp().defaultNow().notNull(),
   },
   (table) => [index("activity_type_idx").on(table.type)],
 );
