4abcb8b4b5865fd3ea9d3d1e5acb5335e567fff4
RvnSytR <ravenskytd17@gmail.com>
2026-08-04 11:46:43 +0700

    deps: update dependencies to latest versions and update next config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  cacheComponents: true,
   typedRoutes: true,
+  cacheComponents: true,
+  partialPrefetching: true,
+
   images: {
     remotePatterns: [
       new URL(`${process.env.S3_ENDPOINT}/**`),
       new URL(`${process.env.S3_PUBLIC_ENDPOINT}/**`),
     ],
   },
+
   experimental: {
     // serverActions: { bodySizeLimit: "5mb" },
     useTypeScriptCli: true,
