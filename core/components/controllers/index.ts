40f6f8f6ac56acbbb1ab7f92e465d6b9ebdc0e10
RvnSytR <ravenskytd17@gmail.com>
2026-08-12 15:07:36 +0700

    chore: remove old data controller module

--- core/components/controllers/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./filters";
-export * from "./page-size";
-export * from "./pagination";
-export * from "./search";
-export * from "./sorting";
-export * from "./visibility";
--- core/utils/index.ts
+++ core/utils/index.ts
@@ -1,7 +1,6 @@
 export * from "./csv";
 export * from "./date";
 export * from "./file";
-export * from "./filters";
 export * from "./helpers";
 export * from "./number";
 export * from "./string";
