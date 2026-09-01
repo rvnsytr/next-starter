357c5c6163833232025f611be62880607520ff76
RvnSytR <ravenskytd17@gmail.com>
2026-09-01 16:20:11 +0700

    feat: add back button on playground page

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -3,14 +3,40 @@ import {
   DashboardPageHeader,
   DashboardPageTitle,
 } from "@/core/components/layout/dashboard-page";
+import { ThemeToggle } from "@/core/components/theme";
+import { Button } from "@/core/components/ui/button";
 import { Separator } from "@/core/components/ui/separator";
+import { LinkSpinner } from "@/core/components/ui/spinner";
+import { cn } from "@/core/utils";
+import { ArrowLeftIcon } from "lucide-react";
 import Link from "next/link";
 import { SaleDataGrid } from "./module/components/sales-tables";
 
 export default function Page() {
   return (
-    <div className="container flex min-h-dvh flex-col gap-y-4 px-0 py-8 lg:border-x">
-      <Separator className="border-t border-dashed bg-transparent" />
+    <div
+      className={cn(
+        "container flex min-h-dvh flex-col gap-y-4 px-0 py-8 lg:border-x",
+        "*:data-[slot=separator]:border-t *:data-[slot=separator]:border-dashed *:data-[slot=separator]:bg-transparent",
+      )}
+    >
+      <Separator />
+
+      <div className="flex justify-between gap-4 px-4">
+        <Button
+          variant="outline"
+          nativeButton={false}
+          render={
+            <Link href="/">
+              <LinkSpinner icon={{ base: <ArrowLeftIcon /> }} /> Back
+            </Link>
+          }
+        />
+
+        <ThemeToggle variant="outline" />
+      </div>
+
+      <Separator />
 
       <DashboardPageHeader className="mx-4">
         <DashboardPageTitle>Data Table</DashboardPageTitle>
@@ -22,15 +48,15 @@ export default function Page() {
         </DashboardPageDescription>
       </DashboardPageHeader>
 
-      <Separator className="border-t border-dashed bg-transparent" />
+      <Separator />
 
       {/* <SaleDataController /> */}
 
-      {/* <Separator className="border-t border-dashed bg-transparent" /> */}
+      {/* <Separator  /> */}
 
       {/* <SaleDataTable /> */}
 
-      {/* <Separator className="border-t border-dashed bg-transparent" /> */}
+      {/* <Separator  /> */}
 
       <SaleDataGrid />
     </div>
